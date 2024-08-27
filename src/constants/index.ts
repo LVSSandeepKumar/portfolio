cde814cf185a7bfbffd8d62c4c6ee79f8c6d270d
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-08-27 15:13:15 +0530

    Featured Projects Section

---
+++ src/constants/index.ts
@@ -0,0 +1,93 @@
+import NikeLandingPage from "@/assets/images/nikelandingpage.png";
+import NextJobs from "@/assets/images/nextjobs.png";
+import TweetSphere from "@/assets/images/tweetsphere.png";
+import FitFusion from "@/assets/images/fit-fusion-ai.png";
+import Flixify from "@/assets/images/flixify.png";
+import FeedbackWidget from "@/assets/images/feedback-widget.png";
+import ReactAdminDashboard from "@/assets/images/react-admin-dashboard.png";
+
+ export const portfolioProjects = [
+    {
+      company: "Personal Project",
+      year: "2024",
+      title: "Nike Landing Page",
+      achievements: [
+        { title: "Device Screen Responsive" },
+        { title: "Sleek and Modern UI" },
+        { title: "Smooth Animations" },
+      ],
+      link: "https://nike-landingpage-ah0k.onrender.com",
+      image: NikeLandingPage,
+    },
+    {
+      company: "Personal Project",
+      year: "2024",
+      title: "Next Jobs",
+      achievements: [
+        { title: "Search, Post & Apply for job features" },
+        { title: "Form actions & validation" },
+        { title: "Improved App usage with NextJS Server Actions" },
+      ],
+      link: "https://next-jobs-ten.vercel.app/",
+      image: NextJobs,
+    },
+    {
+      company: "Personal Project",
+      year: "2024",
+      title: "Tweet Sphere",
+      achievements: [
+        { title: "Register/Login and start posting" },
+        { title: "Photo Storage using Cloudinary" },
+        { title: "Validations using React-Query" },
+      ],
+      link: "https://tweet-sphere.onrender.com",
+      image: TweetSphere,
+    },
+    {
+      company: "Personal Project",
+      year: "2024",
+      title: "Fit Fusion AI",
+      achievements: [
+        { title: "Complex AI Algorithms in application" },
+        { title: "Safe and secure data storage in Postgres" },
+      ],
+      link: "https://fit-fusion-ai-lvs-sandeep-kumar.vercel.app/",
+      image: FitFusion,
+    },
+    {
+      company: "Personal Project",
+      year: "2024",
+      title: "Flixify",
+      achievements: [
+        { title: "Video Streaming using React Player" },
+        { title: "Movie Database from TMDB API" },
+        { title: "Sleek and Attractive UI" },
+      ],
+      link: "https://flixify-netflix-clone.onrender.com",
+      image: Flixify,
+    },
+    {
+      company: "Personal Project",
+      year: "2024",
+      title: "Feedback Widget",
+      achievements: [
+        { title: "Embeddable React Widget Component" },
+        { title: "Personal Dashboard for project details" },
+        { title: "Premium payments using Stripe" },
+      ],
+      link: "https://saas-feedback-dashboard.vercel.app/",
+      image: FeedbackWidget,
+    },
+    {
+      company: "Personal Project",
+      year: "2024",
+      title: "Admin Dashboard",
+      achievements: [
+        { title: "Clean Data Visualisation" },
+        { title: "Bar, Pie & Line Charts using Re-charts" },
+        { title: "Modern and attractive UI" },
+      ],
+      link: "https://react-admin-dashboard-fawn-theta.vercel.app/",
+      image: ReactAdminDashboard,
+    },
+  ];
