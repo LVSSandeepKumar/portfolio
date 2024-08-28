edc255ec10d045779bcaa9d91331f8158f923534
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-08-28 22:39:58 +0530

    Updated Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const calistoga = Calistoga({
 
 export const metadata: Metadata = {
   title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  description: "Watch my works",
 };
 
 export default function RootLayout({
