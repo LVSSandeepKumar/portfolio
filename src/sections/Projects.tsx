fcb7b769fe927d46d60a08f4b2e32bc17ba7b469
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-08-28 20:46:13 +0530

    Project Section Animations

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -15,10 +15,13 @@ export const ProjectsSection = () => {
           description="See how I turned concepts into engaging user experiences."
         />
         <div className="flex flex-col mt-10 md:mt-20 gap-20">
-          {portfolioProjects.map((project) => (
+          {portfolioProjects.map((project, projectIndex) => (
             <Card
               key={project.title}
-              className="px-8 pt-8 pb-0 md:px-10 md:pt-12 lg:pt-16 lg:px-20"
+              className="px-8 pt-8 pb-0 md:px-10 md:pt-12 lg:pt-16 lg:px-20 sticky"
+              style={{
+                top: `calc(64px + ${projectIndex * 40}px)`
+              }}
             >
               <div className="lg:grid lg:grid-cols-2 lg:gap-16">
                 <div className="lg:pb-16">
